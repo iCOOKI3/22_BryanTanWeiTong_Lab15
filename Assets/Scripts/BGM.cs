a501d524c4562888028ed81f9c44b4d6dfdcdef0
iCOOKI3 <bryantan2001@gmail.com>
2021-01-15 23:57:28 +0800

    Tab Audio Change

--- Assets/Scripts/BGM.cs
+++ Assets/Scripts/BGM.cs
@@ -8,14 +8,17 @@ public class BGM : MonoBehaviour
     public AudioClip[] AudioClipArr;
 
     private AudioSource audioSource;
+    private int AudioCounter;
+    private int rand;
     private float volume;
 
     // Start is called before the first frame update
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
-        int rand = Random.Range(0, AudioClipArr.Length);
+        rand = Random.Range(0, AudioClipArr.Length);
         audioSource.PlayOneShot(AudioClipArr[rand]);
+
     }
 
     // Update is called once per frame
@@ -28,6 +31,32 @@ public class BGM : MonoBehaviour
         if(Input.GetKey(KeyCode.DownArrow))
         {
             audioSource.volume = volume-=0.2f * Time.deltaTime;
+        }
+        if(Input.GetKey(KeyCode.Tab))
+        {
+            PlayRandomSong();
+        }
+    }
+
+    private void PlayRandomSong()
+    {
+        if(Input.GetKey(KeyCode.Tab) && rand == 0)
+        {
+            audioSource.Stop();
+            rand = Random.Range(0, AudioClipArr.Length);
+            audioSource.PlayOneShot(AudioClipArr[rand]);
+        }
+        else if(Input.GetKey(KeyCode.Tab) && rand == 1)
+        {
+            audioSource.Stop();
+            rand = Random.Range(0, AudioClipArr.Length);
+            audioSource.PlayOneShot(AudioClipArr[rand]);
+        }
+        else if(Input.GetKey(KeyCode.Tab) && rand ==2)
+        {
+            audioSource.Stop();
+            rand = Random.Range(0, AudioClipArr.Length);
+            audioSource.PlayOneShot(AudioClipArr[rand]);
         }
     }
 }
